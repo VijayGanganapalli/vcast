1f09623ef404ee15de5f1b55189e2bc66561ed4d
Vijay <vijayganganapalli@gmail.com>
2019-04-10 18:30:58 +0530

    added rewind and forward and some features

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,130 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 
-void main() => runApp(Vcast());
+void main() => runApp(MyApp());
 
-class Vcast extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vcast',
-      home: HomePage(),
+      home: BoringPage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class BoringPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: PlayBackButton(),
-      ),
+      body: SafeArea(child: VcastApp()),
     );
   }
 }
 
-class PlayBackButton extends StatefulWidget {
+class VcastApp extends StatelessWidget {
   @override
-  _PlayBackButtonState createState() => _PlayBackButtonState();
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Flexible(
+          flex: 9,
+          child: Placeholder(),
+        ),
+        Flexible(
+          flex: 2,
+          child: AudioControls(),
+        ),
+      ],
+    );
+  }
 }
 
-class _PlayBackButtonState extends State<PlayBackButton> {
+class AudioControls extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        PlayBackButtons(),
+      ],
+    );
+  }
+}
+
+class PlayBackButtons extends StatefulWidget {
+  @override
+  _PlayBackButtonsState createState() => _PlayBackButtonsState();
+}
+
+class _PlayBackButtonsState extends State<PlayBackButtons> {
   String url =
       'http://icdn7.downloadezy.live/2019/Majili/320/01%20-%20One%20Boy%20One%20Girl.mp3';
   FlutterSound _sound;
   bool _isPlaying = false;
+  double _playPosition;
+  Stream<PlayStatus> _playerSubscription;
 
   void _stop() async {
     await _sound.stopPlayer();
+    setState(() => _isPlaying = false);
   }
 
   void _play() async {
     await _sound.startPlayer(url);
+
+    _playerSubscription = _sound.onPlayerStateChanged
+      ..listen((e) {
+        if (e != null) {
+          print(e.currentPosition);
+          setState(() => _playPosition = (e.currentPosition / e.duration));
+        }
+      });
+    setState(() => _isPlaying = true);
   }
+
+  void _fastRewind() {}
+
+  void _fastForward() {}
 
   @override
   void initState() {
     super.initState();
     _sound = FlutterSound();
+    _playPosition = 0.0;
   }
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: _isPlaying ? Icon(Icons.stop) : Icon(Icons.play_arrow),
-      onPressed: () {
-        if (_isPlaying) {
-          _stop();
-        } else {
-          _play();
-        }
-        setState(() => _isPlaying = !_isPlaying);
-      },
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Slider(
+          value: _playPosition,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(Icons.fast_rewind),
+              onPressed: null,
+            ),
+            IconButton(
+              icon: _isPlaying ? Icon(Icons.stop) : Icon(Icons.play_arrow),
+              onPressed: () {
+                if (_isPlaying) {
+                  _stop();
+                } else {
+                  _play();
+                }
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.fast_forward),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
